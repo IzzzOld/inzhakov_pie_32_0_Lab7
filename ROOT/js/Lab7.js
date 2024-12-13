2fdd93e70b0aa0eab71678cd97a0235700c6dadd
IzzzOld <valeravoin32@gmail.com>
2024-12-13 23:30:50 +0700

    Lab7.js

--- ROOT/js/Lab7.js
+++ ROOT/js/Lab7.js
@@ -13,17 +13,22 @@ fetch('https://dummyjson.com/products')
         });
     });
 
-fetch('https://dummyjson.com/products/search?q=apple')
-.then(res => res.json())
-.then(data => {
-    let searchContainer = document.getElementById('searchСontainer');
-    data.products.forEach(function(product) {
-        searchContainer.innerHTML += `
-            <div class="product">
-                <img src="${product.images[0]}">
-                <h3>${product.title}</h3>
-                <h3>Цена: $${product.price}</h3>
-                <h3>Категория: ${product.category}</h3>
-            </div>`;
+function searchProducts() {
+    let searchWord = document.getElementById('searchInput').value;
+    let searchContainer = document.getElementById('searchContainer');
+    searchContainer.innerHTML = ''; // Очистить контейнер перед новым поиском
+
+    fetch(`https://dummyjson.com/products/search?q=${searchWord}`)
+    .then(res => res.json())
+    .then(data => {
+        data.products.forEach(function(product) {
+            searchContainer.innerHTML += `
+                <div class="product">
+                    <img src="${product.images[0]}">
+                    <h3>${product.title}</h3>
+                    <h3>Цена: $${product.price}</h3>
+                    <h3>Категория: ${product.category}</h3>
+                </div>`;
+        });
     });
-});
+}
